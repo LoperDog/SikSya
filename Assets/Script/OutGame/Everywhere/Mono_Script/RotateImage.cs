3b6592753a732e3cea6ec1c6214a446bcc920eea
rnsk1243 <rnsk8945@gmail.com>
2017-10-31 03:46:48 +0900

    돌아가는 ui, room 캐릭터 선택 모션 추가

---
+++ Assets/Script/OutGame/Everywhere/Mono_Script/RotateImage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ConstValueInfo;
+
+public class RotateImage : MonoBehaviour {
+
+    RectTransform mRectTransform;
+
+    // Use this for initialization
+    void Awake () {
+        mRectTransform = this.GetComponent<RectTransform>();
+        //mRectTransform.anchoredPosition = new Vector2(0.5f, 0.5f);
+    }
+
+    public void StartRotate()
+    {
+        StartCoroutine("CoRotateImage");
+    }
+
+    IEnumerator CoRotateImage()
+    {
+        Vector3 rotVec3 = new Vector3();
+        rotVec3.z = -30.0f; 
+        while (true)
+        {
+            mRectTransform.Rotate(rotVec3);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
--- Assets/Script/OutGame/RoomScene/Mono_Script/ChangeCharacter.cs
+++ Assets/Script/OutGame/RoomScene/Mono_Script/ChangeCharacter.cs
@@ -11,14 +11,33 @@ public class ChangeCharacter : MonoBehaviour {
     [SerializeField]
     Sprite[] mSelectCharacterInfoSprite = new Sprite[3];
     Image mSelectCharacterInfo;
+    [SerializeField]
+    GameObject[] mDubu = new GameObject[2];
+    [SerializeField]
+    GameObject[] mMandu = new GameObject[2];
 
     // Use this for initialization
     void Awake () {
         mSender = CSender.GetInstance();
         ChatScript = GameObject.FindGameObjectWithTag("ChatPanel").GetComponent<Chatting>();
         mSelectCharacterInfo = GameObject.FindGameObjectWithTag("TagInfoSelectCharacter").GetComponent<Image>();
+        StartCoroutine("RoomInSelectMotion");
     }
-	
+
+    IEnumerator RoomInSelectMotion()
+    {
+        while(true)
+        {
+            Debug.Log("RoomInSelectMotion 코루틴 호출 됨.");
+            if (MyInfoClass.IsMyInfoInit())
+            {
+                SelectCharacterMotion(ProtocolCharacterImageNameIndex.Tofu);
+                break;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
+    }
 
     public void SelectTofu()
     {
@@ -56,6 +75,39 @@ public class ChangeCharacter : MonoBehaviour {
    //     Debug.Log("ReadyButton누름");
     }
 
+    void SelectCharacterInit()
+    {
+        foreach (GameObject g in mDubu)
+        {
+            g.SetActive(false);
+            g.transform.position = new Vector3();
+        }
+        foreach (GameObject g in mMandu)
+        {
+            g.SetActive(false);
+            g.transform.position = new Vector3();
+        }
+    }
+
+    void SelectCharacterMotion(ProtocolCharacterImageNameIndex characterIndex)
+    {
+        SelectCharacterInit();
+        int team = (MyInfoClass.GetInstance().MyGameNumb % 2); // red : 짝수 , blue : 홀수
+        switch (characterIndex)
+        {
+            case ProtocolCharacterImageNameIndex.Tofu:
+                mDubu[team].SetActive(true);
+                break;
+            case ProtocolCharacterImageNameIndex.Mandu:
+                mMandu[team].SetActive(true);
+                break;
+            case ProtocolCharacterImageNameIndex.Tangsuyuk:
+                break;
+            default:
+                break;
+        }
+    }
+
     void SelectCharacter(ProtocolCharacterImageNameIndex characterIndex)
     {
         Debug.Log("캐릭터 선택 내 상태 = " + CheckState.GetCurState());
@@ -66,6 +118,7 @@ public class ChangeCharacter : MonoBehaviour {
             mSender.Sendn(ref dataInfo);
             CheckState.ChangeState(State.ClientRequestCharacterChange);
             mSelectCharacterInfo.sprite = mSelectCharacterInfoSprite[(int)characterIndex];
+            SelectCharacterMotion(characterIndex);
         }
     }
 
