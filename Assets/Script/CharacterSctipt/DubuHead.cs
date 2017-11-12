66f784b8462ca93ae4133d906fb44f49dab25efe
LoperDog <LoperDog10@gmail.com>
2017-11-12 20:19:08 +0900

    머리테스트

---
+++ Assets/Script/CharacterSctipt/DubuHead.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DubuHead : MonoBehaviour {
+    private CharacterMgr MyMgr;
+    private float LeftBullet;
+	// Use this for initialization
+	void Start () {
+        MyMgr = transform.root.GetComponent<CharacterMgr>();
+        LeftBullet = 0;
+	}
+
+    private void LateUpdate()
+    {
+        LeftBullet= MyMgr.Current_Bullet;
+        float height = LeftBullet / 100;
+        transform.localScale = new Vector3(1.0f, 1.0f - height, 1.0f);
+    }
+}
